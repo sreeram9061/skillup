e4536615d52b8920accf286b05187c779635f7f0
sreeram9061 <sreeramg043@gmail.com>
2023-02-27 16:21:45 -0700

    adding service page in site

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 //this function for when a button hovering then chenge it is bg and color of it is text
 
-hoveringEfect=(bgitem,text,bole)=>{
+hoveringEfect=(fstcol,sedcol,bgitem,text,bole)=>{
    if(bole){
-    console.log("hi")
-    bgitem.style.background="#46ccdb";
-    text.style.color="white";
+    console.log('hei')
+   
+    bgitem.style.background=sedcol;
+    text.style.color=fstcol;
     bgitem.style.transition="all ease-in-out .5s"
     
        
    }else{
        bgitem.style.transition="border ease-in-out .5s"
        bgitem.style.transition="background ease-in-out .6s"
-     bgitem.style.background="white";
+     bgitem.style.background=fstcol;
        bgitem.style.border="1px solid #46ccdb"
-       text.style.color="#46ccdb";
+       text.style.color=sedcol;
        
        
    }
@@ -22,13 +23,13 @@ hoveringEfect=(bgitem,text,bole)=>{
 // navebar right section "contact us" button
 const last_list = document.querySelector('.last_list')
 const last_list_a =document.querySelector('.last_list_a')
-last_list.addEventListener('mouseenter',()=>{ hoveringEfect(last_list,last_list_a,true) })
-last_list.addEventListener('mouseleave',()=>{ hoveringEfect(last_list,last_list_a,false) })
+last_list.addEventListener('mouseenter',()=>{ hoveringEfect("white","#46ccdb",last_list,last_list_a,true) })
+last_list.addEventListener('mouseleave',()=>{ hoveringEfect("white","#46ccdb",last_list,last_list_a,false) })
 
 //home page text btn hover event
 const homepagebtn =document.querySelector('.homepagebtn')
-homepagebtn.addEventListener('mouseenter',()=>{ hoveringEfect(homepagebtn,homepagebtn,true) })
-homepagebtn.addEventListener('mouseleave',()=>{ hoveringEfect(homepagebtn,homepagebtn,false) })
+homepagebtn.addEventListener('mouseenter',()=>{ hoveringEfect("white","#46ccdb",homepagebtn,homepagebtn,true) })
+homepagebtn.addEventListener('mouseleave',()=>{ hoveringEfect("white","#46ccdb",homepagebtn,homepagebtn,false) })
 
 // this section for menu icon 
 const menue =document.querySelector('.menue')
@@ -57,3 +58,29 @@ function navbar_remove_add(bole){
 
 }
 
+//----------------services section-------------------------
+
+const card_1=document.querySelectorAll('.cardcontainer_1');
+
+card_1.forEach((index)=>{
+    let butn
+    let imge
+    index.addEventListener('mouseenter',(ind)=>{
+        butn = ind.target.childNodes[3].childNodes[5]
+        butn.style.background="#46ccdb"
+        butn.childNodes[0].style.color="white"
+        imge =ind.target.childNodes[1].children[0]
+        imge.style.transform="scale(1.1)"
+        
+        let bg_of_cards =ind.target
+        let h4 =ind.target.childNodes[3].childNodes[1]
+        let p =ind.target.childNodes[3].childNodes[3]         
+    })
+    index.addEventListener('mouseleave',(ind)=>{
+        butn.style.background="white"
+        butn.childNodes[0].style.color="#46ccdb"
+        imge.style.transform="scale(1)"
+        
+
+    })
+})
